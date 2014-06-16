15360f36b2f96659ae6d4c45abb2f79322d5f775
fabian achammer <fabian.achammer@gmail.com>
2014-06-16 21:54:18 +0200

    refined throttle method

--- src/cljs/progl/util.cljs
+++ src/cljs/progl/util.cljs
@@ -1,6 +1,6 @@
 (ns progl.util
   (:require-macros [cljs.core.async.macros :as m :refer [go alt!]])
-  (:require [cljs.core.async :as async :refer [chan <! >! mult tap pipe map< alts! timeout take! put!]]))
+  (:require [cljs.core.async :as async :refer [chan <! >! mult tap pipe map< alts! timeout take! put! close!]]))
 
 (defn on-channel [ch f]
   (m/go-loop []
@@ -22,8 +22,8 @@
 (defn throttle [ch delay-time]
   (let [out (chan)]
     (m/go-loop [cur nil]
-               (let [[v] (alts! [ch (timeout delay-time)])]
-                 (when (and cur (not v))
-                   (>! out cur))
-                 (recur v)))
+               (alt!
+                  ch ([v] (if v (recur v) (close! out)))
+                  (timeout delay-time) (when-not (nil? cur) (>! out cur)))
+               (recur cur))
     out))
